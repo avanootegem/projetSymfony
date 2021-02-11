f16f97cdeda0fa845160e165ea275aa5e7a36f4c
avanootegem <aurelien.vanootegem@gmail.com>
2021-02-11 15:34:41 +0100

    return base develop

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Service\CallApiService;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
